a15a923a966c2c6b87bff5ccc5194341aeac250c
camila <camilabentespessoa@gmail.com>
2025-03-30 11:33:53 -0300

    lista de usuarios 01 - apresentando lista em texto

--- src/views/UserList.tsx
+++ src/views/UserList.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 import {Text, View  } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import users from "../data/users";
+
+type User = {
+    id: number;
+    name: string;
+    email: string;
+};
 
 export default function UserList() {
-    return(
+    
+
+    function getUserItem({item}: {item: User}) {
+        return <Text>{item.name} - {item.email}</Text>;
+    }
+
+    return (
         <View>
-        <Text>UserForm</Text>
-        </View>)
+            <FlatList
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
+            />
+        </View>
+    );
 }
