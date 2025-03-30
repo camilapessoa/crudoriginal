0f9e7e2f314f3d4944f4ba097e1c685be934bfa8
camila <camilabentespessoa@gmail.com>
2025-03-30 10:26:17 -0300

    navegacao stack 02 - botao de redirecionamento

--- src/index.tsx
+++ src/index.tsx
@@ -3,26 +3,53 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import UserList from "./views/UserList";
 import UserForm from "./views/UserForm";
+import {TouchableOpacity } from "react-native";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 const Stack = createStackNavigator()
 
-function App() {
+export default function App() {
     return (
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          initialRouteName="UserList"
+          screenOptions={screenOptions}
+        >
           <Stack.Screen
             name="UserList"
             component={UserList}
-            options={{ title: "User List" }}
+            options={({navigation}) => ({
+              title: "Lista de Usuários",
+            headerRight: () =>(
+                <TouchableOpacity
+                onPress={() => navigation.navigate("UserForm")}
+                style={{ marginRight: 15 }}>
+                {/* <AntDesign name="plus" size={30} color="white"  /> */}
+                <FontAwesome6 name="add" size={30} color="white" />
+
+                </TouchableOpacity>
+
+            )
+            
+            })}
+        
           />
           <Stack.Screen
             name="UserForm"
             component={UserForm}
-            options={{ title: "User Form" }}
+            options={{ title: "Formulário de Usuários" }}
+            
           />
         </Stack.Navigator>
       </NavigationContainer>
     );
   }
+
+  const screenOptions = {
+    headerStyle: { backgroundColor: "#f4511e" },
+    headerTintColor: "#fff",
+    headerTitleStyle: { fontWeight: "bold" as "bold" },
+  }
+
   
-  export default App;
